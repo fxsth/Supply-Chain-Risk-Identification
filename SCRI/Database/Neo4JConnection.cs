0d2c270af40ed4717c46586eebfd6c6b9a2ed113
fxsth <fxsth@pm.me>
2021-06-01 13:34:36 +0200

    Projektdateien hinzufügen.

---
+++ SCRI/Database/Neo4JConnection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+
+namespace SCRI.Database
+{
+    class Neo4JConnection : IDisposable
+    {
+        private readonly IDriver _driver;
+        public string connectionStatus { get; set; }
+
+        public Neo4JConnection(string uri, string user, string password)
+        {
+            connectionStatus = "Not connected";
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password), o => o.WithConnectionAcquisitionTimeout(new TimeSpan(3600)));
+        }
+
+        public async Task<string> checkConnectionStatus()
+        {
+            try
+            {
+                var connecting = _driver.VerifyConnectivityAsync();
+                connectionStatus = "Connecting...";
+                await connecting;
+                connectionStatus = "Connected";
+            }
+            catch (Exception e)
+            {
+                connectionStatus = e.Message;
+            }
+            finally
+            {
+                await _driver.CloseAsync();
+            }
+            return connectionStatus;
+        }
+
+        public List<string> GetAllNodeProperties()
+        {
+            var session = _driver.Session();
+            var nodes = session.ReadTransaction(tx => GetAllNodes(tx));
+            return nodes.ConvertAll(
+                new Converter<INode, string>(n => n.Properties.FirstOrDefault().Value.ToString())
+                );
+        }
+
+        private static List<INode> GetAllNodes(ITransaction tx)
+        {
+            var res = tx.Run("MATCH (n) Return n");
+            var list = res.ToList();
+            return list.ConvertAll(
+                new Converter<IRecord, INode>(x => x.Values.Values.First().As<INode>())
+                );
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
---
+++ SCRI/DbConnectionWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SCRI.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SCRI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void onClickConnectAsync(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtURL.Text) || string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
+            {
+                lblStatus.Content = "Missing data to connect";
+                return;
+            }
+            using (var con = new Neo4JConnection(txtURL.Text, txtUsername.Text, txtPassword.Text))
+            {
+                lblStatus.Content = con.connectionStatus;
+                var connecting = con.checkConnectionStatus();
+                lblStatus.Content = con.connectionStatus;
+                lblStatus.Content = await connecting;
+            }
+        }
+    }
+}
